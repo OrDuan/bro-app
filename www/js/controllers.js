e6d6e5d49ebeb879bf2e33dcf0dd68438b54400b
or <orduani@gmail.com>
2016-08-28 17:57:05 +0300

    Add basic chats

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,28 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    .controller('ChatsCtrl', function($scope, Chats, $ionicPlatform, $cordovaMedia) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
+        $scope.chats = Chats.all();
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+        $scope.play = function (type) {
+            var baseSrc = "sounds/" + type + ".wav";
+            var src = ionic.Platform.isAndroid() ? "/android_asset/www/" + baseSrc: baseSrc;
+            var media = new Media(src, null, null, null);
+            media.play();
+        }
+    })
+    
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('AccountCtrl', function($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    });
